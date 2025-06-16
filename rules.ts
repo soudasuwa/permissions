197fef4b7965f25d3f390a9344c8b9edf59b3dcc
Alessandro <108428987+soudasuwa@users.noreply.github.com>
2025-06-16 13:37:55 +0300

    refactor rules hierarchy and enforce complete readonly

--- rules.ts
+++ rules.ts
@@ -2,68 +2,74 @@ import { InvoiceStatus, Operation, Role, type Rule } from "./engine";
 
 export const rules: readonly Rule[] = [
 	{
-		meta: {
-			role: Role.Module,
-			resource: "invoice",
-			operation: Operation.Create,
-		},
-		match: { payload: { status: InvoiceStatus.Generating } },
-	},
-	{
-		meta: {
-			role: Role.Module,
-			resource: "invoice",
-			operation: Operation.Edit,
-		},
-		match: {
-			status: InvoiceStatus.Generating,
-			payload: {
-				status: { in: [InvoiceStatus.Generating, InvoiceStatus.Draft] },
+		meta: { resource: "invoice" },
+		rules: [
+			{
+				meta: { operation: Operation.Edit },
+				match: { status: { not: InvoiceStatus.Complete } },
+				rules: [
+					{
+						meta: { role: Role.Module },
+						match: {
+							status: InvoiceStatus.Generating,
+							payload: {
+								status: {
+									in: [InvoiceStatus.Generating, InvoiceStatus.Draft],
+								},
+							},
+						},
+					},
+					{
+						meta: { role: Role.Admin },
+						match: {
+							status: {
+								in: [InvoiceStatus.Draft, InvoiceStatus.Pending],
+							},
+						},
+					},
+				],
 			},
-		},
-	},
-	{
-		meta: {
-			role: Role.Admin,
-			resource: "invoice",
-			operation: Operation.Create,
-		},
-		match: { payload: { status: { not: InvoiceStatus.Generating } } },
-	},
-	{
-		meta: {
-			role: Role.Admin,
-			resource: "invoice",
-			operation: Operation.Edit,
-		},
-		match: { status: { in: [InvoiceStatus.Draft, InvoiceStatus.Pending] } },
-	},
-	{
-		meta: { role: Role.Admin, resource: "invoice", operation: Operation.View },
-	},
-	{
-		meta: { role: Role.Admin, resource: "invoice", operation: Operation.Pay },
-	},
-	{
-		meta: {
-			role: Role.User,
-			resource: "invoice",
-			operation: Operation.View,
-		},
-		match: {
-			userId: { reference: { actor: "id" } },
-			status: { in: [InvoiceStatus.Pending, InvoiceStatus.Complete] },
-		},
-	},
-	{
-		meta: {
-			role: Role.User,
-			resource: "invoice",
-			operation: Operation.Pay,
-		},
-		match: {
-			userId: { reference: { actor: "id" } },
-			status: InvoiceStatus.Pending,
-		},
+			{
+				meta: { operation: Operation.Create },
+				rules: [
+					{
+						meta: { role: Role.Module },
+						match: { payload: { status: InvoiceStatus.Generating } },
+					},
+					{
+						meta: { role: Role.Admin },
+						match: { payload: { status: { not: InvoiceStatus.Generating } } },
+					},
+				],
+			},
+			{
+				meta: { role: Role.Admin },
+				rules: [
+					{ meta: { operation: Operation.View } },
+					{
+						meta: { operation: Operation.Pay },
+						match: { status: InvoiceStatus.Pending },
+					},
+				],
+			},
+			{
+				meta: { role: Role.User },
+				match: { userId: { reference: { actor: "id" } } },
+				rules: [
+					{
+						meta: { operation: Operation.View },
+						match: {
+							status: {
+								in: [InvoiceStatus.Pending, InvoiceStatus.Complete],
+							},
+						},
+					},
+					{
+						meta: { operation: Operation.Pay },
+						match: { status: InvoiceStatus.Pending },
+					},
+				],
+			},
+		],
 	},
 ];
